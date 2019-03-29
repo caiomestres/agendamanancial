8b9dd304eae67c08c8636aec4df18e42b8e863a6
Caio Mestres <caiodev@gmail.com>
2019-03-29 18:13:11 -0300

    adicionado get de current user+ segredo na keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://caio:picanha12@ds163836.mlab.com:63836/contatos'
+  mongoURI: 'mongodb://caio:picanha12@ds163836.mlab.com:63836/contatos',
+  secretOrKey: 'secret'
 };
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const passport = require('passport');
 const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
-
 //Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,10 +20,11 @@ mongoose
     console.log(err);
   });
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+//Passport middleware
+app.use(passport.initialize());
 
+//Passport Config
+require('./config/passport')(passport);
 // Use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
@@ -32,3 +33,4 @@ app.use('/api/posts', posts);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+8;
